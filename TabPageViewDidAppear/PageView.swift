7c556f95cfa6cb4a98443f7c70bb9dbaf5b2a6d4
Yuta Koshizawa <koher@koherent.org>
2020-10-27 14:32:11 +0900

    Provide `FooViewController` directly in `PageViewController`
    `FooViewController.viewDidAppear` is called.
    
    - `ContentView`
    - `PageView`
    - `PageViewController`
    - `FooViewController`

--- TabPageViewDidAppear/PageView.swift
+++ TabPageViewDidAppear/PageView.swift
@@ -14,8 +14,11 @@ final class PageViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = self
+        let initialViewController = UIHostingController(rootView: FooView(page: 0))
+//        let initialViewController = FooViewController()
+//        initialViewController.page = 0
         setViewControllers(
-            [UIHostingController(rootView: FooView(page: 0))],
+            [initialViewController],
             direction: .forward,
             animated: false,
             completion: nil
@@ -26,11 +29,19 @@ final class PageViewController: UIPageViewController {
 extension PageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         let page = (viewController as! UIHostingController<FooView>).rootView.page
-        return UIHostingController(rootView: FooView(page: page - 1))
+        let nextViewController = UIHostingController(rootView: FooView(page: page - 1))
+//        let page: Int = (viewController as! FooViewController).page
+//        let nextViewController: FooViewController = .init()
+//        nextViewController.page = page - 1
+        return nextViewController
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         let page = (viewController as! UIHostingController<FooView>).rootView.page
-        return UIHostingController(rootView: FooView(page: page + 1))
+        let nextViewController = UIHostingController(rootView: FooView(page: page + 1))
+//        let page: Int = (viewController as! FooViewController).page
+//        let nextViewController: FooViewController = .init()
+//        nextViewController.page = page + 1
+        return nextViewController
     }
 }
